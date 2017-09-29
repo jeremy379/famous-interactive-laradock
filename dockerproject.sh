bcaa089ffe4db3b3d0485d6e312e9ff9667bc4c9
Dillenbourg Jérémy <dillenbourg.jeremy@gmail.com>
2017-09-29 15:05:57 +0200

    Auto add in /etc/hosts

--- dockerproject.sh
+++ dockerproject.sh
@@ -139,3 +139,26 @@ echo "server {
 else
     exit
 fi
+
+# insert/update hosts entry (https://stackoverflow.com/questions/19339248/append-line-to-etc-hosts-file-with-shell-script)
+ip_address="127.0.0.1"
+# find existing instances in the host file and save the line numbers
+matches_in_hosts="$(grep -n $ServerName /etc/hosts | cut -f1 -d:)"
+host_entry="${ip_address} ${ServerName}"
+
+echo "Please enter your password if requested."
+
+if [ ! -z "$matches_in_hosts" ]
+then
+    echo "Updating existing hosts entry."
+    # iterate over the line numbers on which matches were found
+    while read -r line_number; do
+        # replace the text of each line with the desired host entry
+        sudo sed -i '' "${line_number}s/.*/${host_entry} /" /etc/hosts
+    done <<< "$matches_in_hosts"
+else
+    echo "Adding new hosts entry."
+    echo "$host_entry" | sudo tee -a /etc/hosts > /dev/null
+fi
+
+echo "All Done!"
